c03932d37b002adfb11c23f54e633efb0825e04c
Elitina <hei.elitina@gmail.com>
2025-06-23 22:08:27 +0300

    feat: asynchronous reply by email

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloWorldController.java
@@ -1,18 +1,24 @@
 package com.example.demo.endpoint.rest.controller;
 
-import com.example.demo.service.HelloWorldService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.endpoint.event.EventProducer;
+import com.example.demo.endpoint.event.model.SendEmailRequested;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-    private final HelloWorldService service;
+    private final EventProducer<SendEmailRequested> eventProducer;
 
     @GetMapping("/hello")
-    public String helloWorld(@RequestParam String name) {
-        return service.uploadHelloWorldMessage(name);
+    @SneakyThrows
+    public String helloWorld(@RequestParam String to) {
+        var event = SendEmailRequested.builder().to(to).build();
+        eventProducer.accept(List.of(event));
+        return "... world!";
     }
 }
